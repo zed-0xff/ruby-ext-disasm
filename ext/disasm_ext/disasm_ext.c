5835c35868828820adf53f7eede908ebb2a25aa4
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2013-04-19 16:43:50 +0400

    allow disasm syntax change, yield VA

--- ext/disasm_ext/disasm_ext.c
+++ ext/disasm_ext/disasm_ext.c
@@ -8,32 +8,55 @@ static VALUE t_init(VALUE self)
     return INT2FIX(x86_init(opt_none, NULL, NULL));
 }
 
-static VALUE t_disassemble2yield(VALUE self, VALUE _data, VALUE _rva, VALUE _offset)
+static VALUE t_disassemble2yield(VALUE self, VALUE _data, VALUE _rva, VALUE _offset, VALUE _syntax)
 {
     x86_insn_t insn;
     int size, line_len;
     char line[LINE_SIZE];
 
+    if( !_data || _data == Qnil ) return Qnil;
+
     char*buf             = RSTRING_PTR(_data);
     unsigned int bufsize = RSTRING_LEN(_data);
     uint32_t rva         = FIX2INT(_rva);
     unsigned int offset  = FIX2INT(_offset);
+    int syntax           = FIX2INT(_syntax);
+    int n_ok             = 0; // number of successfully disassembled instructions
+
+    if(!buf || !bufsize) return INT2FIX(0);
+
+    switch(syntax){
+        case native_syntax:
+        case intel_syntax:
+        case att_syntax:
+        case xml_syntax:
+        case raw_syntax:
+            break;
+        default:
+            // TODO: raise exception
+            syntax = native_syntax;
+            break;
+    }
 
     while( offset < bufsize ){
         size = x86_disasm(buf, bufsize, rva, offset, &insn);
         if( size ){
             // success
-            line_len = x86_format_insn(&insn, line, LINE_SIZE, intel_syntax);
-            rb_yield(rb_str_new(line, line_len));
+            line_len = x86_format_insn(&insn, line, LINE_SIZE, syntax);
+            rb_yield_values(2, rb_str_new(line, line_len), INT2FIX(offset+rva));
             offset += size;
+            n_ok++;
         } else {
             // invalid instruction
-            rb_yield(INT2FIX(-1));
+            char err_buf[1024];
+            sprintf(err_buf, "raise InvalidInstruction.new(0x%x, 0x%x)", offset, offset+rva);
+            rb_eval_string(err_buf);
+            //rb_raise(ex,"invalid instruction at offset 0x%x (VA 0x%x)", offset, offset+rva);
             break;
         }
     }
 
-    return INT2FIX(bufsize);
+    return INT2FIX(n_ok);
 }
 
 VALUE mDisasm;
@@ -42,5 +65,5 @@ void Init_disasm_ext() {
     x86_init(opt_none, NULL, NULL);
     mDisasm = rb_define_module("Disasm");
     rb_define_singleton_method(mDisasm, "init", t_init, 0);
-    rb_define_singleton_method(mDisasm, "disassemble2yield", t_disassemble2yield, 3);
+    rb_define_singleton_method(mDisasm, "disassemble2yield", t_disassemble2yield, 4);
 }
--- lib/disasm.rb
+++ lib/disasm.rb
@@ -1,18 +1,43 @@
 require 'disasm_ext'
 
 module Disasm
+
+  class Exception < ::Exception; end
+  class InvalidInstruction < Exception
+    attr_accessor :offset, :va
+
+    def initialize offset, va
+      @offset = offset
+      @va = va
+    end
+
+    def to_s
+      "Invalid instruction at offset 0x%x (VA 0x%x)" % [@offset, @va]
+    end
+  end
+
   class << self
     def disasm data, params = {}
-      rva    = params[:rva] || 0
+      rva    = params[:rva] || params[:va] || 0
       offset = params[:offset] || 0
 
+      syntax =
+        case params[:syntax]
+        when :native; 1
+        when :intel;  2
+        when :att;    3
+        when :xml;    4
+        when :raw;    5
+        else 1          # default to native syntax
+        end
+
       if block_given?
-        disassemble2yield(data, rva, offset) do |x|
-          yield x
+        disassemble2yield(data, rva, offset, syntax) do |x,va|
+          yield x,va
         end
       else
         r = []
-        disassemble2yield(data, rva, offset) do |x|
+        disassemble2yield(data, rva, offset, syntax) do |x|
           r << x
         end
         r
