89bad2c8a302651dd1e6d7e6a87574ad171729cf
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2013-04-20 14:35:59 +0400

    extension makefile check for libdisasm

--- ext/disasm_ext/extconf.rb
+++ ext/disasm_ext/extconf.rb
@@ -1,3 +1,11 @@
 require 'mkmf'
-have_library 'disasm'
+
+unless have_library('disasm')
+  abort "libdisasm is missing, please install from http://bastard.sourceforge.net/libdisasm.html"
+end
+
+unless have_header('libdis.h')
+  abort "libdis.h is missing, please install libdisasm from http://bastard.sourceforge.net/libdisasm.html"
+end
+
 create_makefile 'disasm_ext'
